092ee7063349fb003fbfdf4e5ab6fc8f006956fa
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-18 17:47:30 +0530

    FLUT-3233 - Sample and screenshot added.

---
+++ android/app/src/main/kotlin/com/example/vertical_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vertical_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(VerticalPicker());
+
+class VerticalPicker extends StatelessWidget{
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Card(
+            margin: const EdgeInsets.fromLTRB(50, 100, 50, 100),
+            child: SfDateRangePicker(
+              view: DateRangePickerView.month,
+              selectionMode: DateRangePickerSelectionMode.range,
+              enableMultiView: true,
+              navigationDirection: DateRangePickerNavigationDirection.vertical,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
